5c73ac81bd1596f125d11f255ba12268912b515b
vladislav <dincrash@yandex.ru>
2019-12-26 17:29:48 +0300

    change

--- src/main/java/com/wind/Main.java
+++ src/main/java/com/wind/Main.java
@@ -1,16 +1,17 @@
 package com.wind;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+
 public class Main extends JFrame implements ActionListener {
     private static String processName;
     private static String userPC;
-    JTextField tf1, tf2, tf3, tf4, tf5, tf6;
+     JTextField tf1, tf2, tf3, tf4, tf5, tf6;
     JTextArea tf7;
     JLabel q1, q2, q3;
     JButton b1, b2, b3;
@@ -20,22 +21,15 @@ public class Main extends JFrame implements ActionListener {
     private static final String fileName = "C:\\tmp\\2.csv";
 
     public static void main(String[] args) throws Throwable {
+        Main main = new Main();
+//        main.TextFieldExample();
+        main.deleteFile(fileName);
 
-//main.deleteFile(fileName);
-        try {
-            Main main = new Main();
-            main.TextFieldExample();
-            System.out.println("work");
-            // body of main method goes here, including any other error handling
-        } catch (Throwable t) {
-            JOptionPane.showMessageDialog(
-                    null, t.getClass().getSimpleName() + ": " + t.getMessage());
-            throw t; // don't suppress Throwable
-        }
+        System.out.println("work");
+        // body of main method goes here, including any other error handling
     }
 
     public static boolean isProcessRunning(String serviceName, String userPC) throws Exception {
-
         Process p = Runtime.getRuntime().exec(TASKLIST + userPC);
         BufferedReader reader = new BufferedReader(new InputStreamReader(
                 p.getInputStream()));
@@ -53,35 +47,32 @@ public class Main extends JFrame implements ActionListener {
     }
 
     public static void killProcess(String serviceName, String userPC) throws Exception {
-
-//        Runtime.getRuntime().exec(KILL + userPC + "/IM " + serviceName);
-        Runtime.getRuntime().exec("taskkill /s "+userPC +" /IM "+ serviceName);
-
+        Runtime.getRuntime().exec("taskkill /s " + userPC + " /IM " + serviceName);
     }
 
     public void TextFieldExample() throws Exception {
         //путь к папке
         tf1 = new JTextField();
         tf1.setBounds(50, 50, 150, 20);
-//выполненое действие
+        //выполненое действие
         tf2 = new JTextField();
         tf2.setBounds(50, 150, 150, 20);
         tf2.setEditable(false);
-//путь к папке
+        //путь к папке
         tf3 = new JTextField();
         tf3.setBounds(50, 100, 150, 20);
         tf3.setEditable(false);
-//имя заверш процесса
+        //имя заверш процесса
         tf4 = new JTextField();
         tf4.setBounds(250, 50, 150, 20);
         //имя пк
         tf5 = new JTextField();
         tf5.setBounds(250, 100, 150, 20);
-//
+        //
         tf6 = new JTextField();
         tf6.setBounds(250, 150, 150, 20);
         tf6.setEditable(false);
-
+        //вывод tasklist
         tf7 = new JTextArea();
         tf7.setEditable(false);
 
@@ -107,23 +98,20 @@ public class Main extends JFrame implements ActionListener {
         sp.setBounds(50, 350, 400, 200);
         add(sp);
         add(tf1);
-        add(b1);
         add(tf3);
         add(tf2);
         add(tf4);
         add(tf5);
         add(tf6);
-//        add(tf7);
+        add(b1);
         add(b2);
+        add(b3);
         add(q1);
         add(q2);
         add(q3);
-        add(b3);
-
         setSize(500, 800);
         setLayout(null);
         setVisible(true);
-
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -135,7 +123,6 @@ public class Main extends JFrame implements ActionListener {
         s3 = s3.replace('\t', '$');
         String s4 = s3.replaceAll("\\$(.*)", "");
         s4 = "\\" + "\\" + s4 + "\\c$";
-
         if (e.getSource() == b1) {
             System.out.println(s2);
             final File directory = new File(s2);
@@ -165,36 +152,28 @@ public class Main extends JFrame implements ActionListener {
             }
 
         }
-
         //taskkill
         if (e.getSource() == b2) {
             //nameService
             String ss1 = tf4.getText();
             processName = ss1;
-//            processName = "\\" + "\\" + ss1;
-
             //userPC
             String ss2 = tf5.getText();
             userPC = ss2;
             userPC = "\\" + "\\" + ss2;
             try {
                 if (isProcessRunning(processName, userPC)) {
-//                    System.out.println(userPC);
                     killProcess(processName, userPC);
                 }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
-
-
         }
-//tasklist
+        //tasklist
         if (e.getSource() == b3) {
             //nameService
             String ss1 = tf5.getText();
-
             userPC = "\\" + "\\" + ss1;
-
             try {
                 Process p = Runtime.getRuntime().exec(TASKLIST + userPC);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(
@@ -206,7 +185,6 @@ public class Main extends JFrame implements ActionListener {
                     System.out.println(line);
                 }
                 tf7.setText(lines);
-
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
@@ -229,15 +207,47 @@ public class Main extends JFrame implements ActionListener {
         return (directory.delete());
     }
 
-//    public static void deleteFile(String fileName) throws IOException, CsvValidationException {
-//        FileInputStream fis = new FileInputStream(fileName);
-//        InputStreamReader isr = new InputStreamReader(fis, "utf-8");
-//
-//        CSVReader reader = new CSVReader(isr);
-//        String [] nextLine;
-//        while ((nextLine = reader.readNext()) != null) {
-//            // nextLine[] is an array of values from the line
-//            System.out.println(nextLine[0]);
-//        }
-//    }
+    public void deleteFile(String fileName) throws IOException {
+        BufferedReader csvReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName),"Cp1251"));
+        String row ;
+
+        while ((row = csvReader.readLine()) != null) {
+            row = row.replace(':', '$');
+            row = row.replace(';', '\\');
+
+            String s4 = row.replaceAll("\\\\(.*)", "");
+            s4 = "\\" + "\\" + s4 + "\\c$";
+            row = "\\" + "\\" + row;
+            final File directory = new File(row);
+            final File pk = new File(s4);
+            if (pk.exists()) {
+                if (directory.exists()) {
+                    Runtime.getRuntime().exec("TAKEOWN /F "+ directory);
+                    String exec = "icacls "+ directory+" /grant \"domain users\":F";
+                    Runtime.getRuntime().exec(exec);
+                    deleteDirectory(directory);
+                    if (directory.exists()) {
+                        String result = "Процесс запущен";
+                        System.out.println(result+"/n "+row);
+                    } else {
+                        String result = "Папка удалена";
+                        System.out.println(result+"/n "+row);
+
+                    }
+                } else {
+                    String result = "Папки(Файла) нет";
+                    System.out.println(result+"/n "+row);
+
+
+                }
+            } else {
+                String result = "ПК не вкл";
+                System.out.println(result+"/n "+row);
+
+            }
+
+        }
+
+        csvReader.close();
+    }
 }
