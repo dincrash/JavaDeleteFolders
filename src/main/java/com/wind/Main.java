eb7b93785f58d7f5e88b74edef0ebaf7511ece64
vladislav <dincrash@yandex.ru>
2019-12-31 12:36:42 +0300

    Revert "Revert "change""
    This reverts commit 52fdca8f67d75cf67930de9f9f23b074db8551cb.

--- src/main/java/com/wind/Main.java
+++ src/main/java/com/wind/Main.java
@@ -1,194 +1,32 @@
 package com.wind;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
-
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.io.*;
 
-public class Main extends JFrame implements ActionListener {
-    private static String processName;
-    private static String userPC;
-     JTextField tf1, tf2, tf3, tf4, tf5, tf6;
-    JTextArea tf7;
-    JLabel q1, q2, q3;
-    JButton b1, b2, b3;
-    public String s2;
-    private static final String TASKLIST = "TASKLIST /s ";
-    private static final String KILL = "TASKKILL /s ";
+public class Main extends JFrame {
     private static final String fileName = "C:\\tmp\\2.csv";
+    private static final String path = "C:\\tmp\\otchet.txt";
 
     public static void main(String[] args) throws Throwable {
         Main main = new Main();
-//        main.TextFieldExample();
-        main.deleteFile(fileName);
-
-        System.out.println("work");
-        // body of main method goes here, including any other error handling
+//        main.deleteFile(fileName);
+        main.TFFrame();
     }
 
-    public static boolean isProcessRunning(String serviceName, String userPC) throws Exception {
-        Process p = Runtime.getRuntime().exec(TASKLIST + userPC);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                p.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null) {
-
-            System.out.println(line);
-            if (line.contains(serviceName)) {
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    public static void killProcess(String serviceName, String userPC) throws Exception {
-        Runtime.getRuntime().exec("taskkill /s " + userPC + " /IM " + serviceName);
-    }
-
-    public void TextFieldExample() throws Exception {
-        //путь к папке
-        tf1 = new JTextField();
-        tf1.setBounds(50, 50, 150, 20);
-        //выполненое действие
-        tf2 = new JTextField();
-        tf2.setBounds(50, 150, 150, 20);
-        tf2.setEditable(false);
-        //путь к папке
-        tf3 = new JTextField();
-        tf3.setBounds(50, 100, 150, 20);
-        tf3.setEditable(false);
-        //имя заверш процесса
-        tf4 = new JTextField();
-        tf4.setBounds(250, 50, 150, 20);
-        //имя пк
-        tf5 = new JTextField();
-        tf5.setBounds(250, 100, 150, 20);
-        //
-        tf6 = new JTextField();
-        tf6.setBounds(250, 150, 150, 20);
-        tf6.setEditable(false);
-        //вывод tasklist
-        tf7 = new JTextArea();
-        tf7.setEditable(false);
-
-        b1 = new JButton("Удалить");
-        b1.setBounds(50, 200, 100, 50);
-        b1.addActionListener(this);
-
-        b2 = new JButton("taskkill");
-        b2.setBounds(250, 275, 180, 50);
-        b2.addActionListener(this);
-
-        b3 = new JButton("tasklist");
-        b3.setBounds(50, 275, 180, 50);
-        b3.addActionListener(this);
-
-        q1 = new JLabel("Путь к папке удаления");
-        q1.setBounds(50, 30, 150, 20);
-        q2 = new JLabel("Имя завершаемого процесса");
-        q2.setBounds(250, 30, 180, 20);
-        q3 = new JLabel("Имя компьютера");
-        q3.setBounds(250, 80, 150, 20);
-        JScrollPane sp = new JScrollPane(tf7);
-        sp.setBounds(50, 350, 400, 200);
-        add(sp);
-        add(tf1);
-        add(tf3);
+    public void TFFrame() {
+        TextField tf = new TextField("Выполнено");
+        TextField tf2 = new TextField("Сюда 3 столбика C:\\tmp\\2.csv");
+        TextField tf3 = new TextField("Тут отчет C:\\tmp\\otchet.txt");
+        tf.setBounds(50, 50, 100, 25);
+        tf2.setBounds(50, 75, 200, 25);
+        tf3.setBounds(50, 100, 200, 25);
+        add(tf);
         add(tf2);
-        add(tf4);
-        add(tf5);
-        add(tf6);
-        add(b1);
-        add(b2);
-        add(b3);
-        add(q1);
-        add(q2);
-        add(q3);
-        setSize(500, 800);
+        add(tf3);
+        setSize(300, 300);
         setLayout(null);
         setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        String s1 = tf1.getText();
-        s2 = "\\" + "\\" + s1;
-        s2 = s2.replace(':', '$');
-        s2 = s2.replace('\t', '\\');
-        String s3 = s1;
-        s3 = s3.replace('\t', '$');
-        String s4 = s3.replaceAll("\\$(.*)", "");
-        s4 = "\\" + "\\" + s4 + "\\c$";
-        if (e.getSource() == b1) {
-            System.out.println(s2);
-            final File directory = new File(s2);
-            final File pk = new File(s4);
-            if (pk.exists()) {
-                if (directory.exists()) {
-                    deleteDirectory(directory);
-                    if (directory.exists()) {
-                        String result = "Процесс запущен";
-                        tf3.setText(result);
-                        tf2.setText(s2);
-                    } else {
-                        String result = "Папка удалена";
-                        tf3.setText(result);
-                        tf2.setText(s2);
-                    }
-                } else {
-                    String result = "Папки(Файла) нет";
-                    tf3.setText(result);
-                    tf2.setText(s2);
-
-                }
-            } else {
-                String result = "ПК не вкл";
-                tf3.setText(result);
-                tf2.setText(s4);
-            }
-
-        }
-        //taskkill
-        if (e.getSource() == b2) {
-            //nameService
-            String ss1 = tf4.getText();
-            processName = ss1;
-            //userPC
-            String ss2 = tf5.getText();
-            userPC = ss2;
-            userPC = "\\" + "\\" + ss2;
-            try {
-                if (isProcessRunning(processName, userPC)) {
-                    killProcess(processName, userPC);
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        //tasklist
-        if (e.getSource() == b3) {
-            //nameService
-            String ss1 = tf5.getText();
-            userPC = "\\" + "\\" + ss1;
-            try {
-                Process p = Runtime.getRuntime().exec(TASKLIST + userPC);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        p.getInputStream()));
-                String line;
-                String lines = "";
-                while ((line = reader.readLine()) != null) {
-                    lines += "\n" + line;
-                    System.out.println(line);
-                }
-                tf7.setText(lines);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 
     public boolean deleteDirectory(File directory) {
@@ -207,47 +45,88 @@ public class Main extends JFrame implements ActionListener {
         return (directory.delete());
     }
 
-    public void deleteFile(String fileName) throws IOException {
-        BufferedReader csvReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName),"Cp1251"));
-        String row ;
-
-        while ((row = csvReader.readLine()) != null) {
-            row = row.replace(':', '$');
-            row = row.replace(';', '\\');
-
-            String s4 = row.replaceAll("\\\\(.*)", "");
-            s4 = "\\" + "\\" + s4 + "\\c$";
-            row = "\\" + "\\" + row;
-            final File directory = new File(row);
-            final File pk = new File(s4);
-            if (pk.exists()) {
-                if (directory.exists()) {
-                    Runtime.getRuntime().exec("TAKEOWN /F "+ directory);
-                    String exec = "icacls "+ directory+" /grant \"Domain Users\":F";
-                    Runtime.getRuntime().exec(exec);
-                    deleteDirectory(directory);
+    public void deleteFile(String fileName) throws Exception {
+        try {
+            BufferedReader csvReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "Cp1251"));
+            String row;
+            String UserPCrow;
+            String PCprocess;
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            writer.write("");
+            writer.close();
+            while ((row = csvReader.readLine()) != null) {
+                row = row.replace(':', '$');
+                row = row.replace(';', '\\');
+                String s4 = row.replaceAll("\\\\(.*)", "");
+                String PC = s4;
+                s4 = "\\" + "\\" + s4 + "\\c$";
+                UserPCrow = "\\" + "\\" + row;
+                PCprocess = row.replaceAll("(.*)\\\\", "");
+                final File directory = new File(UserPCrow);
+                final File pk = new File(s4);
+                if (pk.exists()) {
                     if (directory.exists()) {
-                        String result = "Процесс запущен";
-                        System.out.println(result+"/n "+row);
-                    } else {
-                        String result = "Папка удалена";
-                        System.out.println(result+"/n "+row);
+                        String psexec = "wmic /node: \"" + PC + "\" Path win32_process Where \"CommandLine Like '%" + PCprocess + "%'\" Call Terminate";
+                        Runtime.getRuntime().exec("TAKEOWN /F " + directory);
+                        String exec = "icacls " + directory + " /grant \"Domain Users\":F";
+                        //kill proccess wmic /node: "c000006660" Path win32_process Where "CommandLine Like '%excel%'" Call Terminate
+                        Runtime.getRuntime().exec(psexec);
+                        deleteDirectory(directory);
+                        if (directory.exists()) {
+                            Runtime.getRuntime().exec(exec);
+                            Thread.sleep(2000);
+                            deleteDirectory(directory);
+                            Thread.sleep(5000);
 
+                            if (directory.exists()) {
+                                String result = "Удалено";
+                                writer = new BufferedWriter(new FileWriter(path, true));
+                                writer.write(result + UserPCrow);
+                                writer.newLine();
+                                writer.close();
+
+                                System.out.println(result + UserPCrow);
+                            } else {
+                                String noDel = "Не удаляется";
+                                writer = new BufferedWriter(new FileWriter(path, true));
+                                writer.write(noDel + UserPCrow);
+                                writer.newLine();
+                                writer.close();
+
+                                System.out.println(noDel + UserPCrow);
+                            }
+                        } else {
+                            String result = "Удалено";
+                            writer = new BufferedWriter(new FileWriter(path, true));
+                            writer.write(result + UserPCrow);
+                            writer.newLine();
+                            writer.close();
+
+                            System.out.println(result + UserPCrow);
+                        }
+                    } else {
+                        String result = "Папки(Файла) нет";
+                        writer = new BufferedWriter(new FileWriter(path, true));
+                        writer.write(result + UserPCrow);
+                        writer.newLine();
+                        writer.close();
+
+                        System.out.println(result + UserPCrow);
                     }
                 } else {
-                    String result = "Папки(Файла) нет";
-                    System.out.println(result+"/n "+row);
+                    String result = "ПК не вкл";
+                    writer = new BufferedWriter(new FileWriter(path, true));
+                    writer.write(result + UserPCrow);
+                    writer.newLine();
+                    writer.close();
 
-
+                    System.out.println(result + UserPCrow);
                 }
-            } else {
-                String result = "ПК не вкл";
-                System.out.println(result+"/n "+row);
-
             }
-
+            csvReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        csvReader.close();
     }
 }
